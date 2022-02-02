4a93079f1f64e788ece339466e46d7c91568faaa
Venu Vardhan Reddy Tekula <venuvardhanreddytekula8@gmail.com>
2022-02-02 17:09:42 +0530

    Rename configurations to jwt_drf_playground

--- jwt_drf_playground/asgi.py
+++ jwt_drf_playground/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for oauth_drf_playground project.
+ASGI config for jwt_drf_playground project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oauth_drf_playground.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwt_drf_playground.settings')
 
 application = get_asgi_application()
--- jwt_drf_playground/wsgi.py
+++ jwt_drf_playground/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for oauth_drf_playground project.
+WSGI config for jwt_drf_playground project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oauth_drf_playground.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwt_drf_playground.settings')
 
 application = get_wsgi_application()
